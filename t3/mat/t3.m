8ed7c707a8067e996a04af355dc73ddb22ece45b
Francisco-Mendes-96529 <francisco.m.b.mendes@tecnico.ulisboa.pt>
2021-04-30 00:58:33 +0100

    merit 22

---
+++ t3/mat/t3.m
@@ -0,0 +1,27 @@
+close all
+clear all
+
+%%EXAMPLE SYMBOLIC COMPUTATIONS
+
+pkg load symbolic
+
+
+
+
+
+
+
+%hf = figure ();
+%plot (f, 180/pi*(angle(vc) + pi), "m");
+%hold on;
+%plot (f, av6, "b");
+%hold on;
+%plot (f, 180/pi*angle(vs), "r");
+
+%legend("vc","v6","vs");
+%xlabel ("log_{10}(f) [Hz]");
+%ylabel ("Phase v_c(f), v_6(f), v_s(f) [degrees]");
+%print (hf, "phase.eps", "-depsc");
+%system("epstopdf phase.eps");
+%close(hf);
+%disp("\nfigure saved");
